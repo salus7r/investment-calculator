7c85b87a20d632c183cc652a1981ebdf5985830a
Salman Zahid Latif <salu_s7r@yahoo.com>
2019-09-15 22:14:57 +0500

    Revert "package updates and configuration"
    This reverts commit db58315c5fa087776026ef332b99ade2840b58a7.

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from "react-hot-loader";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// Wrap the rendering in a function:
-const render = () => {
-  ReactDOM.render(
-    // Wrap App inside AppContainer
-    <AppContainer>
-      <App />
-    </AppContainer>,
-    document.getElementById("root")
-  );
-};
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// Do this once
-// serviceWorker.unregister();
-serviceWorker.register();
-
-// Render once
-render();
-
-// Webpack Hot Module Replacement API
-if ((module as any).hot) {
-  (module as any).hot.accept("./App", () => {
-    render();
-  });
-}
+serviceWorker.unregister();
